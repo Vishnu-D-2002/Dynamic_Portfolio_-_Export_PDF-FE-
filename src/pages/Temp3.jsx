3664dc6e11dffd3fec15854e310c39d85977dcec
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-20 01:36:59 +0530

    temp3 done

--- src/pages/Temp3.jsx
+++ src/pages/Temp3.jsx
@@ -1,72 +1,187 @@
+import React, { useState } from 'react';
+import './Temp3.css';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
-import React from 'react';
-import './Temp2.css';
+const Temp3 = () => {
+  const [userImage, setUserImage] = useState(null);
 
-const Resume = () => {
- return (
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setUserImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const resumeData = JSON.parse(sessionStorage.getItem('resumeData'));
+
+  const {
+    studentDetails,
+    educationDetails,
+    additionalDetails,
+    personalDetails,
+    experience,
+  } = resumeData;
+
+  const downloadPDF = () => {
+    const capture = document.querySelector('.resume-container3');
+
+    html2canvas(capture).then((canvas) => {
+      const imgData = canvas.toDataURL('img/png');
+      const doc = new jsPDF('p', 'mm', 'a4');
+      const componentWidth = doc.internal.pageSize.getWidth();
+      const componentHeight = doc.internal.pageSize.getHeight();
+      doc.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight);
+      doc.save('Resume.pdf');
+    });
+  };
+
+  return (
     <div>
-      {/* Education Details */}
-      <h2>Education Details:</h2>
-      <p>College Name: {details.educationDetails.college.collegeName}</p>
-      <p>Qualification: {details.educationDetails.college.qualification}</p>
-      <p>CGPA: {details.educationDetails.college.cgpa}</p>
-      <p>Passed Out Year: {details.educationDetails.college.passedOutYear}</p>
-
-      <p>Tenth School Name: {details.educationDetails.tenth.schoolName}</p>
-      <p>Board: {details.educationDetails.tenth.board}</p>
-      <p>Percentage: {details.educationDetails.tenth.percentage}</p>
-      <p>Passed Out Year: {details.educationDetails.tenth.passedOutYear}</p>
-
-      <p>Twelfth School Name: {details.educationDetails.twelfth.schoolName}</p>
-      <p>Board: {details.educationDetails.twelfth.board}</p>
-      <p>Percentage: {details.educationDetails.twelfth.percentage}</p>
-      <p>Passed Out Year: {details.educationDetails.twelfth.passedOutYear}</p>
-
-      {/* Personal Details */}
-      <h2>Personal Details:</h2>
-      <p>Date of Birth: {details.personalDetails.DateofBirth}</p>
-      <p>Father's Name: {details.personalDetails["Father'sName"]}</p>
-      <p>Gender: {details.personalDetails.Gender}</p>
-      <p>Language Proficiency: {details.personalDetails.LanguageProficiency}</p>
-      <p>Marital Status: {details.personalDetails.MaritalStatus}</p>
-      <p>Nationality: {details.personalDetails.Nationality}</p>
-      <p>Native Place: {details.personalDetails.NativePlace}</p>
-
-      {/* Student Details */}
-      <h2>Student Details:</h2>
-      <p>Address: {details.studentDetails.address}</p>
-      <p>Email: {details.studentDetails.email}</p>
-      <p>First Name: {details.studentDetails.firstname}</p>
-      <p>Last Name: {details.studentDetails.lastname}</p>
-      <p>Phone Number: {details.studentDetails.phonenumber}</p>
-      <p>Github: {details.studentDetails.github}</p>
-
-      {/* Additional Details */}
-      <h2>Additional Details:</h2>
-      <p>Area of Interest 1: {details.additionalDetails.areaOfInterest1}</p>
-      <p>Area of Interest 2: {details.additionalDetails.areaOfInterest2}</p>
-      {/* ... repeat for other areas of interest */}
+      <h2>If you want Image in your resume Please upload the Image in below input box </h2>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
       
-      <p>Career Objective: {details.additionalDetails.careerObjective}</p>
+      <button onClick={downloadPDF}>Download</button>
+      <div id='box3'>
+        <div className="resume-container3">
 
-      {/* Skills */}
-      <h3>Skills:</h3>
-      {Object.values(details.additionalDetails)
-        .filter(value => value.startsWith('skills'))
-        .map((skill, index) => (
-          <p key={index}>Skill {index + 1}: {skill}</p>
-        ))}
+          <div className="left-side3">
+            
+            <div id='top-center3'>
+            {userImage && <img src={userImage} alt="Uploaded" width={130} height={130} id='img3' />}
+            <strong className='underline3 top3'>
+              {studentDetails.firstname} {studentDetails.lastname}
+            </strong>
+            <a className='top3' href={`mailto:${studentDetails.email}`}>
+              {studentDetails.email}
+            </a>
+            <p className='underline3 top3' id='phone3'>{studentDetails.phonenumber}</p>
+            </div>
+            
+            <div className="section3">
+              <h2 className='underline3'>Career Objective: </h2>
+              <p className='line-height3'>{additionalDetails.careerObjective}</p><hr />
 
-      {/* Personal Skills */}
-      <h3>Personal Skills:</h3>
-      {Object.values(details.additionalDetails)
-        .filter(value => value.startsWith('personalSkill'))
-        .map((personalSkill, index) => (
-          <p key={index}>Personal Skill {index + 1}: {personalSkill}</p>
-        ))}
+              <h2 className='underline3'>Contact Details:</h2>
+              {Object.keys(studentDetails).map((key, index) => (
+                // Check if the value is not empty before rendering
+                studentDetails[key].trim() !== '' && (
+                  <p key={index}>
+                    {key.charAt(0).toUpperCase() + key.slice(1)}: {studentDetails[key]}
+                  </p>
+                )
+              ))}
+            </div><hr />
+
+            <div className="section3">
+              <h2 className='underline3'>Personal Details:</h2>
+              <p>Date of Birth: {personalDetails.dob}</p>
+              <p>Father's Name: {personalDetails.fathersName}</p>
+              <p>Gender: {personalDetails.gender}</p>
+              <p>Language Proficiency: {personalDetails.languageProficiency}</p>
+              <p>Marital Status: {personalDetails.maritalStatus}</p>
+              <p>Nationality: {personalDetails.nationality}</p>
+              <p>Native Place: {personalDetails.placeOfBirth}</p>
+            </div><hr />
+
+            <div className="section3">
+              <h2 className='underline3'>Skills:</h2>
+              <ul className="detail-list3">
+                {Object.keys(additionalDetails)
+                  .filter((key) => key.startsWith('skills') && additionalDetails[key].trim() !== '')
+                  .map((key) => (
+                    <li key={key} className='line-height3'>{additionalDetails[key]}</li>
+                  ))}
+              </ul><hr />
+
+              <h2 className='underline3'>Personal Skills:</h2>
+              <ul className="detail-list3">
+                {Object.keys(additionalDetails)
+                  .filter(
+                    (key) =>
+                      key.startsWith('personalSkill') && additionalDetails[key].trim() !== ''
+                  )
+                  .map((key) => (
+                    <li key={key} className='line-height3'>{additionalDetails[key]}</li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="right-side3">
+            <div className="section3">
+              <h2 className='underline3'>Education Details:</h2>
+              <h2 className='head-left3'>{educationDetails.college.qualification}</h2>
+              <h3 className='black3'> {educationDetails.college.collegeName}</h3>
+              <h3 className='black3'>{educationDetails.college.cgpa}% - {educationDetails.college.passedOutYear}</h3>
+
+              <h2 className='head-left3'>SSLC - {educationDetails.tenth.board}</h2>
+              <h3 className='black3'> {educationDetails.tenth.schoolName}</h3>
+              <h3 className='black3'>{educationDetails.tenth.percentage}% - {educationDetails.tenth.passedOutYear}</h3>
+
+              <h2 className='head-left3'>HSC - {educationDetails.twelfth.board}</h2>
+              <h3 className='black3'>{educationDetails.twelfth.schoolName}</h3>
+              <h3 className='black3'>{educationDetails.twelfth.percentage}% - {educationDetails.twelfth.passedOutYear}</h3>
+            </div><hr />
+
+           
+            <div className="section3">
+              {experience.experiences.some(exp => exp.title.trim() !== '' || exp.description.trim() !== '') && (
+                <h2 className='underline3'>Projects:</h2>
+              )}
+
+              {experience.projects.map((project, index) => (
+                <div key={index}>
+                  <h2 className='head-left3'>{project.title}</h2>
+                  <p id='project3' className='line-height3'>{project.description}</p>
+                </div>
+              ))}<hr />
+
+               <div className="section3">
+              {experience.experiences.some(exp => exp.title.trim() !== '' || exp.description.trim() !== '') && (
+                <h2 className='underline3'>Experience:</h2>
+              )}
+
+              {experience.experiences.map((exp, index) => (
+                <div key={index}>
+                  <h2 className='head-left3'>{exp.title}</h2>
+                  <p id='experience3' className='line-height3'>{exp.description}</p>
+                </div>
+              ))}
+            </div><hr />
+
+            <div className="section3">
+              <h2 className='underline3'>Hobbies:</h2>
+              <ul>
+                {experience.hobbies.map((hobby, index) => (
+                  <li key={index} className='line-height3'>{hobby.name}</li>
+                ))}
+              </ul>
+            </div><hr />
+
+              <h2 className='underline3'>Area of Interest:</h2>
+              <ul className="detail-list3">
+                {Object.keys(additionalDetails)
+                  .filter(
+                    (key) =>
+                      key.startsWith('areaOfInterest') && additionalDetails[key].trim() !== ''
+                  )
+                  .map((key) => (
+                    <li key={key} className='line-height3'>{additionalDetails[key]}</li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Resume;
-
+export default Temp3;
