94ac0c3353f4d1700aed3f4f0e7fba3677917d84
Vishnu-D-2002 <vishnulosangels@gmail.com>
2023-12-20 13:18:00 +0530

    temp4

--- src/pages/Temp4.jsx
+++ src/pages/Temp4.jsx
@@ -42,155 +42,155 @@ const Temp4 = () => {
     });
   };
 
-  return (
-    <div>
-      <h2>If you want Image in your resume Please upload the Image in below input box </h2>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
+    return (
+        <div>
+            <h2>If you want Image in your resume Please upload the Image in below input box </h2>
+            <input type="file" accept="image/*" onChange={handleImageUpload} />
       
-      <button onClick={downloadPDF}>Download</button>
-      <div id='box4'>
-        <div className="resume-container4">
+            <button onClick={downloadPDF}>Download</button>
+            <div id='box4'>
+                <div className="resume-container4">
 
-          <div className="left-side4">
+                    <div className="right-side4">
             
-            <div id='top-center4'>
-            {userImage && <img src={userImage} alt="Uploaded" width={130} height={130} id='img4' />}
-            <strong className='underline4 top4'>
-              {studentDetails.firstname} {studentDetails.lastname}
-            </strong>
-            <a className='top4' href={`mailto:${studentDetails.email}`}>
-              {studentDetails.email}
-            </a>
-            <p className='underline4 top4' id='phone4'>{studentDetails.phonenumber}</p>
-            </div>
+                        <div id='top-center4'>
+                            {userImage && <img src={userImage} alt="Uploaded" width={130} height={130} id='img4' />}
+                            <strong className='underline4 top4'>
+                                {studentDetails.firstname} {studentDetails.lastname}
+                            </strong>
+                            <a className='top4' href={`mailto:${studentDetails.email}`}>
+                                {studentDetails.email}
+                            </a>
+                            <p className='underline4 top4' id='phone4'>{studentDetails.phonenumber}</p>
+                        </div>
             
-            <div className="section4">
+                        <div className="section4">
               
 
-              <h2 className='underline4'>Contact Details:</h2>
-              {Object.keys(studentDetails).map((key, index) => (
-                // Check if the value is not empty before rendering
-                studentDetails[key].trim() !== '' && index>3 && (
-                  <p key={index}>
-                    {key.charAt(0).toUpperCase() + key.slice(1)}: {studentDetails[key]}
-                  </p>
-                )
-              ))}
-            </div><hr />
+                            <h2 className='underline4'>Contact Details:</h2>
+                            {Object.keys(studentDetails).map((key, index) => (
+                                // Check if the value is not empty before rendering
+                                studentDetails[key].trim() !== '' && index > 3 && (
+                                    <p key={index} className='ml4'>
+                                        {key.charAt(0).toUpperCase() + key.slice(1)}: {studentDetails[key]}
+                                    </p>
+                                )
+                            ))}
+                        </div><hr />
 
-            <div className="section4">
-              <h2 className='underline4'>Personal Details:</h2>
-              <p>Date of Birth: {personalDetails.dob}</p>
-              <p>Father's Name: {personalDetails.fathersName}</p>
-              <p>Gender: {personalDetails.gender}</p>
-              <p>Language Proficiency: {personalDetails.languageProficiency}</p>
-              <p>Marital Status: {personalDetails.maritalStatus}</p>
-              <p>Nationality: {personalDetails.nationality}</p>
-              <p>Native Place: {personalDetails.placeOfBirth}</p>
-                      </div><hr />
+                        <div className="section4">
+                            <h2 className='underline4'>Personal Details:</h2>
+                            <p className='ml4'>Date of Birth: {personalDetails.dob}</p>
+                            <p className='ml4'>Father's Name: {personalDetails.fathersName}</p>
+                            <p className='ml4'>Gender: {personalDetails.gender}</p>
+                            <p className='ml4'>Language Proficiency: {personalDetails.languageProficiency}</p>
+                            <p className='ml4'>Marital Status: {personalDetails.maritalStatus}</p>
+                            <p className='ml4'>Nationality: {personalDetails.nationality}</p>
+                            <p className='ml4'>Native Place: {personalDetails.placeOfBirth}</p>
+                        </div><hr />
                       
-                       <div className="section4">
-              <h2 className='underline4'>Hobbies:</h2>
-              <ul>
-                {experience.hobbies.map((hobby, index) => (
-                  <li key={index} className='line-height4'>{hobby.name}</li>
-                ))}
-              </ul>
-            </div><hr />
+                        <div className="section4">
+                            <h2 className='underline4'>Hobbies:</h2>
+                            <ul>
+                                {experience.hobbies.map((hobby, index) => (
+                                    <li key={index} className='line-height4'>{hobby.name}</li>
+                                ))}
+                            </ul>
+                        </div><hr />
 
-              <h2 className='underline4'>Area of Interest:</h2>
-              <ul className="detail-list4">
-                {Object.keys(additionalDetails)
-                  .filter(
-                    (key) =>
-                      key.startsWith('areaOfInterest') && additionalDetails[key].trim() !== ''
-                  )
-                  .map((key) => (
-                    <li key={key} className='line-height4'>{additionalDetails[key]}</li>
-                  ))}
-              </ul>
+                        <h2 className='underline4'>Area of Interest:</h2>
+                        <ul className="detail-list4">
+                            {Object.keys(additionalDetails)
+                                .filter(
+                                    (key) =>
+                                        key.startsWith('areaOfInterest') && additionalDetails[key].trim() !== ''
+                                )
+                                .map((key) => (
+                                    <li key={key} className='line-height4'>{additionalDetails[key]}</li>
+                                ))}
+                        </ul><hr />
 
-                    <div className="section4">
+                        <div className="section4">
               
 
-              <h2 className='underline4'>Personal Skills:</h2>
-              <ul className="detail-list4">
-                {Object.keys(additionalDetails)
-                  .filter(
-                    (key) =>
-                      key.startsWith('personalSkill') && additionalDetails[key].trim() !== ''
-                  )
-                  .map((key) => (
-                    <li key={key} className='line-height4'>{additionalDetails[key]}</li>
-                  ))}
-              </ul>
-                          </div>
+                            <h2 className='underline4'>Personal Skills:</h2>
+                            <ul className="detail-list4">
+                                {Object.keys(additionalDetails)
+                                    .filter(
+                                        (key) =>
+                                            key.startsWith('personalSkill') && additionalDetails[key].trim() !== ''
+                                    )
+                                    .map((key) => (
+                                        <li key={key} className='line-height4'>{additionalDetails[key]}</li>
+                                    ))}
+                            </ul>
+                        </div>
                           
-          </div>
+                    </div>
 
-                  <div className="right-side4">
+                    <div className="left-side4">
                       
-                      <h2 className='underline4'>Career Objective: </h2>
-                      <p className='line-height4'>{additionalDetails.careerObjective}</p><hr />
+                        <h2 className='underline4'>Career Objective: </h2>
+                        <p className='line-height4'>{additionalDetails.careerObjective}</p><hr />
                       
-            <div className="section4">
-              <h2 className='underline4'>Education Details:</h2>
-              <h2 className='head-left4'>{educationDetails.college.qualification}</h2>
-              <h3 className='black4'> {educationDetails.college.collegeName}</h3>
-              <h3 className='black4'>{educationDetails.college.cgpa}% - {educationDetails.college.passedOutYear}</h3>
+                        <div className="section4">
+                            <h2 className='underline4'>Education Details:</h2>
+                            <h2 className='head-left4'>{educationDetails.college.qualification}</h2>
+                            <h3 className='black4'> {educationDetails.college.collegeName}</h3>
+                            <h3 className='black4'>{educationDetails.college.cgpa}% - {educationDetails.college.passedOutYear}</h3>
 
-              <h2 className='head-left4'>SSLC - {educationDetails.tenth.board}</h2>
-              <h3 className='black4'> {educationDetails.tenth.schoolName}</h3>
-              <h3 className='black4'>{educationDetails.tenth.percentage}% - {educationDetails.tenth.passedOutYear}</h3>
+                            <h2 className='head-left4'>SSLC - {educationDetails.tenth.board}</h2>
+                            <h3 className='black4'> {educationDetails.tenth.schoolName}</h3>
+                            <h3 className='black4'>{educationDetails.tenth.percentage}% - {educationDetails.tenth.passedOutYear}</h3>
 
-              <h2 className='head-left4'>HSC - {educationDetails.twelfth.board}</h2>
-              <h3 className='black4'>{educationDetails.twelfth.schoolName}</h3>
-              <h3 className='black4'>{educationDetails.twelfth.percentage}% - {educationDetails.twelfth.passedOutYear}</h3>
-            </div><hr />
+                            <h2 className='head-left4'>HSC - {educationDetails.twelfth.board}</h2>
+                            <h3 className='black4'>{educationDetails.twelfth.schoolName}</h3>
+                            <h3 className='black4'>{educationDetails.twelfth.percentage}% - {educationDetails.twelfth.passedOutYear}</h3>
+                        </div><hr />
 
            
-                      <div className="section4">
+                        <div className="section4">
                           
-                          <h2 className='underline4'>Skills:</h2>
-              <ul className="detail-list4">
-                {Object.keys(additionalDetails)
-                  .filter((key) => key.startsWith('skills') && additionalDetails[key].trim() !== '')
-                  .map((key) => (
-                    <li key={key} className='line-height4'>{additionalDetails[key]}</li>
-                  ))}
-                          </ul><hr />
+                            <h2 className='underline4'>Skills:</h2>
+                            <ul className="detail-list4">
+                                {Object.keys(additionalDetails)
+                                    .filter((key) => key.startsWith('skills') && additionalDetails[key].trim() !== '')
+                                    .map((key) => (
+                                        <li key={key} className='line-height4'>{additionalDetails[key]}</li>
+                                    ))}
+                            </ul><hr />
                           
-              {experience.experiences.some(exp => exp.title.trim() !== '' || exp.description.trim() !== '') && (
-                <h2 className='underline4'>Projects:</h2>
-              )}
+                            {experience.experiences.some(exp => exp.title.trim() !== '' || exp.description.trim() !== '') && (
+                                <h2 className='underline4'>Projects:</h2>
+                            )}
 
-              {experience.projects.map((project, index) => (
-                <div key={index}>
-                  <h2 className='head-left4'>{project.title}</h2>
-                  <p id='project4' className='line-height4'>{project.description}</p>
-                </div>
-              ))}<hr />
+                            {experience.projects.map((project, index) => (
+                                <div key={index}>
+                                    <h2 className='head-left4'>{project.title}</h2>
+                                    <p id='project4' className='line-height4 ml4'>{project.description}</p>
+                                </div>
+                            ))}<hr />
 
-               <div className="section4">
-              {experience.experiences.some(exp => exp.title.trim() !== '' || exp.description.trim() !== '') && (
-                <h2 className='underline4'>Experience:</h2>
-              )}
+                            <div className="section4">
+                                {experience.experiences.some(exp => exp.title.trim() !== '' || exp.description.trim() !== '') && (
+                                    <h2 className='underline4 '>Experience:</h2>
+                                )}
 
-              {experience.experiences.map((exp, index) => (
-                <div key={index}>
-                  <h2 className='head-left4'>{exp.title}</h2>
-                  <p id='experience4' className='line-height4'>{exp.description}</p>
-                </div>
-              ))}
-            </div><hr />
+                                {experience.experiences.map((exp, index) => (
+                                    <div key={index}>
+                                        <h2 className='head-left4'>{exp.title}</h2>
+                                        <p id='experience4' className='line-height4 ml4'>{exp.description}</p>
+                                    </div>
+                                ))}
+                            </div>
 
                        
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Temp4;
